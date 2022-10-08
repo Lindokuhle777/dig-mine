ffdb1436fdb1323c6f4531d2c8c8267ae37ba285
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-10-08 14:05:20 +0200

    Home route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div>
-      Home
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
