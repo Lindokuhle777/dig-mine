89c49e21937e173441f72e49b75236624a49fc4e
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-10-08 19:55:39 +0200

    Background color

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Feed from '../Feed/Feed';
 import Package from '../Packages/Package';
+import { Toolbar, AppBar, Button } from '@mui/material';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -19,8 +20,8 @@ function TabPanel(props) {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+        <Box style={{ position: 'absolute', left: "20%", backgroundColor: '#404755' }}>
+          <div>{children}</div>
         </Box>
       )}
     </div>
@@ -48,29 +49,35 @@ export default function VerticalTabs() {
   };
 
   return (
-    <Box
-      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}
+    <div
     >
-      <Tabs
-        orientation="vertical"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider' }}
-      >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-      </Tabs>
+      <div style={{ position: 'absolute', margin: 0, left: 0, width: "20%", backgroundColor: '#404755', height: "100vh" }}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <Typography variant='h4'>Mining Exchange</Typography>
+        </div>
+
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          style={{ position: 'absolute', margin: 0, left: 0, width: "100%", height: "100vh" }}
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs></div>
+
       <TabPanel value={value} index={0}>
-        <Feed/>
+        <Feed />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Package/>
+        <Package />
       </TabPanel>
       <TabPanel value={value} index={2}>
         Item Three
       </TabPanel>
-    </Box>
+
+    </div>
   );
 }
